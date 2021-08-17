3a77ee694bbc0a8daca987f655ba2846c7d780a6
Gilberto Navarro <71095837+Gilnavarro1@users.noreply.github.com>
2021-08-16 21:44:33 -0300

    Add files via upload

---
+++ Aula04/AppHeranca.java
@@ -0,0 +1,20 @@
+package Aula04;
+
+public class AppHeranca {
+
+  public static void main(String[] args) {
+    Pessoa p = new Pessoa("Nome1", "email1@email.com");
+
+    System.out.println(p.toString());
+
+    Funcionario f = new Funcionario("Nome2", "email2@email.com", 7000);
+    System.out.println(f.toString());
+    f.setNome("Nome do funcionário");
+    System.out.println(f.toString());
+
+    Funcionario f2 = new Funcionario("Nome3", "email3@email.com", 20, 6500);
+    System.out.println(f2.toString());
+
+  }
+
+}
